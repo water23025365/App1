47d81c55b1fbbacffe5dbb31123d601ceb69be65
文祺 林 <delphi@live.shu.edu.tw>
2018-07-23 17:41:32 +0800

    test

---
+++ App1/App1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App1
+{
+    class Class1
+    {
+        public static Color LightBackground = Color.Yellow;
+        public static Color DarkForeground = Color.Blue;
+        public static double NormalFontSize = 18;
+        public static double TitleFontSize = 1.4 * NormalFontSize;
+        public static double ParagraphSpacing = 10;
+        public const FontAttributes Emphasis = FontAttributes.Italic;
+        public const FontAttributes TitleAttribute = FontAttributes.Bold;
+        public const TextAlignment TitleAlignment = TextAlignment.Center;
+    }
+}
---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            Label label = new Label
+            {
+                Text = "Opacity Binding Demo",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+        }
+	}
+}
