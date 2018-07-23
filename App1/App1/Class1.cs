565c546b58d0a925a728851b8f5d1de6cda5e60a
文祺 林 <delphi@live.shu.edu.tw>
2018-07-23 17:46:46 +0800

    test

--- App1/App1/Class1.cs
+++ App1/App1/Class1.cs
@@ -11,9 +11,10 @@ namespace App1
         public static Color DarkForeground = Color.Blue;
         public static double NormalFontSize = 18;
         public static double TitleFontSize = 1.4 * NormalFontSize;
-        public static double ParagraphSpacing = 10;
+        public static double ParagraphSpacing = 11;
         public const FontAttributes Emphasis = FontAttributes.Italic;
         public const FontAttributes TitleAttribute = FontAttributes.Bold;
         public const TextAlignment TitleAlignment = TextAlignment.Center;
+
     }
 }
